ac51d76377635dfa523c805991a05c694b88980f
Diego González Suárez <UO276406@uniovi.es>
2023-02-05 10:51:41 +0100

    Carga datos

--- keywordextractor.py
+++ keywordextractor.py
@@ -1,70 +1,51 @@
-import pickle
-
 import numpy as np
-from graphtext import GraphText
+
+from spacy.lang.en.stop_words import STOP_WORDS
 
 
 class KeywordExtractor:
     """Extract keywords from text"""
-    def __init__(self, damping_factor=0.85, convergence_threshold=1e-5, iteration_steps=50, processed_text=None):
+    def __init__(self, damping_factor=0.85, convergence_threshold=1e-5, iteration_steps=50, processor=None, graph=None):
         self.damping_factor = damping_factor  # usually  .85
         self.threshold = convergence_threshold
         self.iterations = iteration_steps
-        self.processed_text = processed_text
-
-    """Creates the graph for the algorithm"""
-    def create_graph(self):
-
-        """
-        # Builds the graph
-        graphtext = GraphText(self.processed_text.vocabulary, self.processed_text.processed_text)
-        graphtext.create_graph()
-
-        # Saves the graph in memory
-        with open('graph0.pickle', 'wb') as handle:
-            pickle.dump(graphtext, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        """
-        with open('graph0.pickle', 'rb') as handle:
-          graphtext = pickle.load(handle)
-
-        return graphtext
+        self.processor = processor
+        self.graph = graph
 
     """Gives a score for every word which has been found in the vocabulary"""
-    def score_vocabulary_words(self, graphtext):
-        vocab_len = len(self.processed_text.vocabulary)
+    def score_vocabulary_words(self):
+        vocab_len = len(self.processor.vocabulary)
 
         # Gets the sum of weights of every vertex
         inout = np.zeros(vocab_len, dtype='f')
         for i in range(0, vocab_len):
             for j in range(0, vocab_len):
-                inout[i] += graphtext.weighted_edge[i][j]
+                inout[i] += self.graph.weighted_edge[i][j]
 
         for iteration in range(0, self.iterations):
-            prev_score = np.copy(graphtext.score)
+            prev_score = np.copy(self.graph.score)
             for i in range(0, vocab_len):
                 summation = 0
 
                 for j in range(0, vocab_len):
-                    if graphtext.weighted_edge[i][j] != 0:
-                        summation += (graphtext.weighted_edge[i][j] / inout[j]) * graphtext.score[j]
-                graphtext.score[i] = (1 - self.damping_factor) + self.damping_factor * summation
+                    if self.graph.weighted_edge[i][j] != 0:
+                        summation += (self.graph.weighted_edge[i][j] / inout[j]) * self.graph.score[j]
+                self.graph.score[i] = (1 - self.damping_factor) + self.damping_factor * summation
 
-            if np.sum(np.fabs(prev_score - graphtext.score)) <= self.threshold:  # convergence condition
+            if np.sum(np.fabs(prev_score - self.graph.score)) <= self.threshold:  # convergence condition
                 print("Converging at iteration " + str(iteration) + "...")
                 break
 
-        print("Scores for single words done")
-
-    """Gets keyphrases from the text"""
+    """Gets keyphrases susceptible of being keywords"""
     def get_candidate_keyphrases(self):
         candidate_phrases = []
         candidate_phrase = " "
-        for word in self.processed_text.lemmatized_text:
-            if word in self.processed_text.stopwords:
+        for word in self.processor.lemmatized_text:
+            if word in self.processor.stopwords or word in STOP_WORDS:
                 if candidate_phrase != " ":
                     candidate_phrases.append(str(candidate_phrase).strip().split())
                 candidate_phrase = " "
-            elif word not in self.processed_text.stopwords:
+            elif word not in self.processor.stopwords and word not in STOP_WORDS:
                 candidate_phrase += str(word)
                 candidate_phrase += " "
 
@@ -74,7 +55,7 @@ class KeywordExtractor:
             if phrase not in unique_phrases:
                 unique_phrases.append(phrase)
 
-        for word in self.processed_text.vocabulary:
+        for word in self.processor.vocabulary:
             for phrase in unique_phrases:
                 if (word in phrase) and ([word] in unique_phrases) and (len(phrase) > 1):
                     # if len(phrase)>1 then the current phrase is multi-worded.
@@ -83,10 +64,10 @@ class KeywordExtractor:
                     # then I will remove the single-word-phrase from the list.
                     unique_phrases.remove([word])
 
-        # print(unique_phrases)
         return unique_phrases
 
-    def score_keyphrases(self, phrases, graphtext):
+    """Score the keyphrases selected"""
+    def score_keyphrases(self, phrases):
         phrase_scores = []
         keywords = []
         for phrase in phrases:
@@ -95,35 +76,39 @@ class KeywordExtractor:
             for word in phrase:
                 keyword += str(word)
                 keyword += " "
-                if word in self.processed_text.vocabulary:
-                    phrase_score += graphtext.score[self.processed_text.vocabulary.index(word)]
+                if word in self.processor.vocabulary:
+                    phrase_score += self.graph.score[self.processor.vocabulary.index(word)]
+
             phrase_scores.append(phrase_score)
             keywords.append(keyword.strip())
-        i = 0
-        for keyword in keywords:
-            print("Keyword: '" + str(keyword) + "', Score: " + str(phrase_scores[i]))
-            i += 1
+        """
+        for i in range(len(keywords)):
+            if phrase_scores[i] > 0:
+                print("Keyword [" + str(i) + "] : " + str(keywords[i]) + ", Score: " + str(phrase_scores[i]))
+        """
+
         return keywords, phrase_scores
 
     """Returns the keywords of the text"""
-    def get_keywords(self, text):
-        # Creates the graph for the text and execute the algorithm
-        graphtext = self.create_graph()
-
+    def get_keywords(self):
         # calculates the punctuation
-        self.score_vocabulary_words(graphtext)
+        self.score_vocabulary_words()
 
         # gets candidate phrases
         phrases = self.get_candidate_keyphrases()
 
         # scores all the phrases
-        keywords, phrase_scores = self.score_keyphrases(phrases, graphtext)
+        keywords, phrase_scores = self.score_keyphrases(phrases)
 
         # Get ranked keywords
+        """
         sorted_index = np.flip(np.argsort(phrase_scores), 0)
-        keywords_num = 10
+        keywords_num = 100
         print("Keywords found:\n")
         for i in range(0, keywords_num):
             print(str(keywords[sorted_index[i]]))
+        """
+        return keywords
+
 
 
--- main.py
+++ main.py
@@ -1,17 +1,55 @@
+from graph.graph import Graph
 from keywordextractor import KeywordExtractor
-from textprocessor import TextProcessor
+from processor.textprocessor import TextProcessor
+import pickle
 
 
-f = open("./data/History/dataSet/USHist_0.txt", 'r', encoding='utf8')
-text = f.read()
-f.close()
+def main():
+    for i in range(0, 10):
+        f = open("./data/History5/dataSet/From yesterday to tomorrow _ history and citizenship education_glossary_" + str(i) + ".txt", 'r', encoding='utf8')
+        text = f.read()
+        f.close()
+        # text = "Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types systems and systems of mixed types."
+        # nltk.download('averaged_perceptron_tagger')
+        # nltk.download('wordnet')
 
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('wordnet')
+        # Process the text
+        processor = TextProcessor(text)
+        processor.process_text()
 
-# Process the text
-processed_text = TextProcessor(text)
-processed_text.process_text()
+        # ----------------------------------------------------------------------
+        # Creates the graph
 
-extractor = KeywordExtractor(processed_text=processed_text)
-extractor.get_keywords(text)
+        graph = Graph(processor.vocabulary, processor.filtered_text)
+        graph.create_graph()
+        # Saves the graph in memory
+        with open('./graphsaved/History5/From yesterday to tomorrow _ history and citizenship education_glossary_' + str(i) + '.pickle', 'wb') as handle:
+            pickle.dump(graph, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    exit()
+
+    """
+    # ----------------------------------------------------------------------
+    # Loads the graph from memory
+    graph = None
+    with open('./graphsaved/graph' + str(i) + '.pickle', 'rb') as handle:
+        graph = pickle.load(handle)
+    # ----------------------------------------------------------------------
+
+    # Looks for the keywords
+    extractor = KeywordExtractor(processor=processor, graph=graph, damping_factor=0.85,
+                                 convergence_threshold=1e-2, iteration_steps=10)
+    keywords_found = extractor.get_keywords()
+
+    # Count the keywords found
+    key = open("./data/History/dataSet/USHist_" + str(i) + ".key", 'r', encoding='utf8')
+    keywords_real = key.readlines()
+    key.close()
+
+    print("Keywords found for USHist_" + str(i))
+    print(keywords_found)
+    print()
+    """
+
+
+if __name__ == "__main__":
+    main()
